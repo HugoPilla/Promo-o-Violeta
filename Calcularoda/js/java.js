644f3d6e4d4994207ddc1470c5fecb09a28b1eb6
HugoPilla <105288596+HugoPilla@users.noreply.github.com>
2022-05-24 09:13:39 -0300

    Add files via upload

--- Calcularoda/js/java.js
+++ Calcularoda/js/java.js
@@ -47,10 +47,32 @@ for(var i = 0; i<btn.length; i++){
     }           
     
 };
-btn[i].addEventListener('click', function(){
+btn[i].addEventListener("click", function(){
     var btnVal = this.innerHTML, 
     inputVal = input.innerHTML;
     console.log(btnVal);
+
+    switch(btnVal){
+        case "c":
+            input.innerHTML = "";
+            break;
+            case"=":
+            var equacao = inputVal;
+
+            if(equacao){
+                try{
+                    input.innerHTML = eval(equacao);
+            }   catch (e){
+                alert ("Erro na Expressao ")
+            } 
+
+    }
+    break;
+    default:
+    input.innerHTML += btnVal;
+    break;
+}
+
 })
 
     }
